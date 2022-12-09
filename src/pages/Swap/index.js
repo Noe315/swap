ec3db478e75cd505e64a51c8ec403894dfb686e4
Dat Nguyen <eminemempire3@gmail.com>
2022-12-09 23:51:43 +0700

    feat: add uniswap SDK

--- src/pages/Swap/index.js
+++ src/pages/Swap/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components/macro';
 import Button from 'react-bootstrap/Button';
 import BoxInput from '../../components/BoxInput';
@@ -6,7 +6,12 @@ import BoxInfo from '../../components/BoxInfo';
 import BoxWrapper from '../../components/BoxWrapper';
 import { TableHeader } from '../../components/styles';
 import ModalSlippage from '../../components/ModalSlippage';
-import { DEFAULT_SLIPPAGE } from '../../constants/address';
+import { DEFAULT_SLIPPAGE, KAI_MAINNET_CHAIN_ID } from '../../constants/address';
+import { Fetcher, Fraction, Percent, Route, Token, TokenAmount, Trade, TradeType } from '@uniswap/sdk';
+import { Network } from "@ethersproject/networks";
+import { ethers } from 'ethers';
+import { getWeb3 } from '../../utils/connectWallet';
+import JSBI from 'jsbi';
 
 export default function Swap () {
   const [isInfo, setIsInfo] = useState(false);
@@ -23,6 +28,12 @@ export default function Swap () {
   const [isAddressSame, setIsAddressSame] = useState(false);
   const isInputValid = useRef();
   const isOutputValid = useRef();
+  const [web3, setWeb3] = useState();
+
+  useEffect(() => {
+    const _web3 = getWeb3();
+    setWeb3(_web3);
+  }, []);
 
   const inputOnChange = (event) => {
     const value = event.target.value;
@@ -168,6 +179,67 @@ export default function Swap () {
     shouldApproveButtonDisabled();
   }
 
+  const getInfo = async () => {
+    const BN = web3.utils.BN;
+    const tokenA = new Token(KAI_MAINNET_CHAIN_ID, '0xb4D6438ebBB73bfF7Aea4E66d2B98469B6Ae4DEf', 3);
+    const tokenB = new Token(KAI_MAINNET_CHAIN_ID, '0xbf35A89559F5e746cb8921F81dcd276612B41387', 3);
+    const tokenC = new Token(KAI_MAINNET_CHAIN_ID, '0x72E184cf075EB1CFA861B49eC4E88E2311150a94', 3);
+    const tokenD = new Token(KAI_MAINNET_CHAIN_ID, '0x34e4b4994DEF3Df13481E430a4c7Eb0e476e0A25', 3);
+    const tokenE = new Token(KAI_MAINNET_CHAIN_ID, '0x7F240E9885E0e1Cc485EBBA51181BB7033b8e7B6', 18);
+    const tokenTest = new Token(KAI_MAINNET_CHAIN_ID, '0xeD40c77CEd39E37180357Fd0C7f7A5C98f214B94', 3);
+
+    const network = {
+      name: 'KardiaChain',
+      chainId: KAI_MAINNET_CHAIN_ID,
+      _defaultProvider: (providers) => new providers.JsonRpcProvider('https://rpc.kardiachain.io')
+    }
+    const provider = ethers.getDefaultProvider(network);
+
+    const pairs = [];
+
+    const pair0 = await Fetcher.fetchPairData(tokenA, tokenB, provider);
+    const pair1 = await Fetcher.fetchPairData(tokenA, tokenTest, provider);
+    const pair2 = await Fetcher.fetchPairData(tokenD, tokenC, provider);
+    const pair3 = await Fetcher.fetchPairData(tokenC, tokenA, provider);
+    const pair4 = await Fetcher.fetchPairData(tokenE, tokenA, provider);
+    const pair5 = await Fetcher.fetchPairData(tokenC, tokenB, provider);
+
+    pairs[0] = pair0;
+    pairs[1] = pair1;
+    pairs[2] = pair2;
+    pairs[3] = pair3;
+    pairs[4] = pair4;
+    pairs[5] = pair5;
+
+    const trade = Trade.bestTradeExactIn(pairs, new TokenAmount(tokenA, '1000'), tokenE, { maxNumResults: 3, maxHops: 3 });
+    const denominatorBN = new BN(trade[0].priceImpact.denominator).toString();
+    const priceImpact = trade[0].priceImpact.toFixed(2, { groupSeparator: ',' });
+    // const minOut = trade.minimumAmountOut(new Percent('50', '10000'));
+    // const fraction = Fraction(trade[0].priceImpact.numerator, trade[0].priceImpact.denominator).toFixed(2, { groupSeparator: ',' });
+    const input = trade[0].inputAmount.toFixed(3, { groupSeparator: ',' });
+    const output = trade[0].outputAmount.toFixed(5, { groupSeparator: ',' });
+    const inOverOut = input / output;
+    const outOverIn = output / input;
+
+    console.log(
+      'pairs: ', pairs,
+      // ' route: ', route,
+      ' trade: ', trade,
+      ' denominatorBN: ', denominatorBN,
+      ' trade[0].priceImpact.denominator: ', JSON.stringify(trade[0].priceImpact.denominator),
+      ' trade[0].priceImpact.numerator: ', JSON.stringify(trade[0].priceImpact.numerator),
+      ' priceImpact: ', priceImpact,
+      ' outputAmount: ', trade[0].outputAmount.toFixed(5, { groupSeparator: ',' }),
+      ' executionPrice: ', trade[0].executionPrice.toFixed(3, { groupSeparator: ',' }),
+      ' input: ', input,
+      ' output: ', output,
+      ' inOverOut: ', inOverOut,
+      ' outOverIn: ', outOverIn,
+      // ' minOut: ', minOut,
+      // ' fraction: ', fraction,
+    );
+  };
+
   return (
     <BoxWrapper>
       <TableHeader>
@@ -191,6 +263,7 @@ export default function Swap () {
           } %
         </div>
       </TableHeader>
+      <Button onClick={getInfo}>Get info</Button>
       <Button onClick={
           () => console.log(
             'slippage: ',
